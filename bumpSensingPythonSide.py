fc9f97f4e381ea96ab180d53d1576ce54e65f0af
carlmstott <124844573+carlmstott@users.noreply.github.com>
2023-08-29 11:55:59 -0500

    Update bumpSensingPythonSide.py
    changed code a little, gave up on taking an OOP approach

--- bumpSensingPythonSide.py
+++ bumpSensingPythonSide.py
@@ -9,16 +9,6 @@ leftMotor=int(100)
 rightMotor=int(100)
 
 
-class Robotstate:
-    def on_enter(self):
-        pass
-    
-    def on_exit(self):
-        pass  
-
-class driving(Robotstate):
-    pass
-
 if __name__ == '__main__':
     ser=serial.Serial('/dev/ttyACM0',115200)
     #every time the serial port is opened, the arduino program will restart
@@ -34,10 +24,9 @@ if __name__ == '__main__':
 
         if ready ==1:
             sendString('/dev/ttyACM0',115200,'<'+str(leftMotor)+','+str(rightMotor)+'>',0.0005)
-            # ser.write(b'<'+bytes(str(leftMotor),'utf-8')+b','+bytes(str(rightMotor),'utf-8')+b'>')
-            #interestingly ser.write is sending proper data around 95% of the time in this function,
-            #and sending data 1 byte at a time using sendString is causing the 
-            # print('<'+str(leftMotor)+','+str(rightMotor)+'>')
+
+
+            
             if ser.in_waiting > 0:  
                 line = ser.readline().decode('utf-8')
                 # print(line)
